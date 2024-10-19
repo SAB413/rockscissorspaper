c88898968baa725c9b39dcda3474a580f8d85444
SAB413 <scott@barnettonline.net>
2024-10-19 14:16:55 +1100

    mostly tidying and refactoring
    added emojis, play again button and played with
    some different ways of doing eventlisteners to
    reduce duplicate code

--- SBscript.js
+++ SBscript.js
@@ -3,65 +3,62 @@
 let humanScore = 0;
 let computerScore = 0;
 let outcomestr = "none"; 
+const MAX_SCORE=5;
+const body = document.querySelector("body");
 const resulttext = document.querySelector("#result");
 const finalresult = document.querySelector("#finalresult");
 resulttext.textContent = "...";
 const humanscore = document.querySelector("#humanscore");
 const compscore = document.querySelector("#compscore");
 
-const btnrock = document.querySelector("#rock");
-const btnscissors = document.querySelector("#scissors");
-const btnpaper = document.querySelector("#paper");
+const btns = document.querySelector("#playbuttons");
 
-btnrock.addEventListener('click', (e) => {
-    outcomestr = playRound("rock");
+// next step to change this from an inline/anonymous function to a callback function
+
+btns.addEventListener('click', (e) => {
+    const isButton = e.target.nodeName === 'BUTTON';
+    if (!isButton) return;
+    if (!(outcomestr === "none")) return;
+    outcomestr = playRound(e.target.id);
+    if (!(outcomestr === "none")) {
+        // game ended - add button to play again
+        // just doing in js for practice - probably better to define in .html/css and then just enable/disable in js
+        const div = document.createElement("div");
+        const btnpa = document.createElement("button");
+
+        div.setAttribute("id","playagaindiv");
+        div.setAttribute("class","resultdivs");
+        div.style.display = "flex";
+        div.style.alignItems = "center";
+        div.style.justifyContent = "center";
+        btnpa.textContent = "Play again";
+        btnpa.style.width = "200px";
+        btnpa.addEventListener('click', Restart);
+        
+        div.appendChild(btnpa);
+        body.appendChild(div);
+    }
     if (outcomestr ==="human") {
         finalresult.textContent = "Human Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
     }
     else if (outcomestr ==="comp") {
         finalresult.textContent = "Computer Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
-    }       
+    } 
 }); 
 
 
-btnscissors.addEventListener('click', (e) => {
-    outcomestr = playRound ("scissors");
-    if (outcomestr ==="human") {
-        finalresult.textContent = "Human Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
-    }
-    else if (outcomestr ==="comp") {
-        finalresult.textContent = "Computer Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
-    }
-});
-    
-
-btnpaper.addEventListener('click', (e) => {
-    outcomestr = playRound ("paper");
-    if (outcomestr ==="human") {
-        finalresult.textContent = "Human Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
-    }
-    else if (outcomestr ==="comp") {
-        finalresult.textContent = "Computer Wins. Press <<Play Again>>";
-        btnrock.disabled = true;
-        btnscissors.disabled = true;
-        btnpaper.disabled = true;
-    }
-});
+function Restart(e) {
+    console.log(e);
+    humanScore = 0;
+    computerScore = 0;
+    outcomestr = "none";
+    humanscore.textContent = ` ${humanScore}`;
+    compscore.textContent = ` ${computerScore}`;
+    resulttext.textContent = "...";
+    finalresult.textContent = "";
+    const thisdiv = document.querySelector("#playagaindiv");
+    body.removeChild(thisdiv);
+}
 
 
 function playRound(humanChoice) {
@@ -73,7 +70,7 @@ function playRound(humanChoice) {
             console.log("You win! " + humanChoice + " beats "+computerChoice+".");
             humanScore++;
             humanscore.textContent = ` ${humanScore}`;
-            if (humanScore==5) return("human");
+            if (humanScore==MAX_SCORE) return("human");
         }
     else if ((humanChoice === 'rock' && computerChoice ==='paper') ||
     (humanChoice === 'scissors' && computerChoice ==='rock') ||
@@ -82,13 +79,13 @@ function playRound(humanChoice) {
         console.log("You lose! " + computerChoice + " beats "+humanChoice+".");
         computerScore++;
         compscore.textContent = ` ${computerScore}`;
-        if (computerScore==5) return("comp");
+        if (computerScore==MAX_SCORE) return("comp");
     }
     else {
         resulttext.textContent = "It's a draw. "+ computerChoice + " matches "+humanChoice+".";
         console.log("It's a draw. "+ computerChoice + " matches "+humanChoice+".");
-        return("none");
    }
+   return ("none");
 }
 
 function getComputerChoice () {
